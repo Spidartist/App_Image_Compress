e1ccf2954fffdddbc4c430d09ee38f43eb47b298
Spidartist <72654978+Spidartist@users.noreply.github.com>
2022-04-06 08:51:24 +0700

    Update compress.py

--- compress.py
+++ compress.py
@@ -66,6 +66,9 @@ if uploaded_file is not None:
     st.subheader("Result")
     display_img = np.concatenate(all_images, axis=1)
     st.image(display_img / 255.0, channels="BGR", clamp=True)
+    
+    result = Image.fromarray(cropped_image.astype('uint8'), 'RGB')
+    img = Image.open(result)
     btn = st.download_button(
              label="Download compressed image",
              data=result,
